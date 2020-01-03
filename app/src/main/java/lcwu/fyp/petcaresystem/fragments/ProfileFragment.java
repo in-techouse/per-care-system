277e7c961431b2debf342e573a98610e0e8d6dec
zofis <zofishanmaqbool@gmail.com>
2020-01-03 13:23:57 +0500

    User Profile

--- app/src/main/java/lcwu/fyp/petcaresystem/fragments/ProfileFragment.java
+++ app/src/main/java/lcwu/fyp/petcaresystem/fragments/ProfileFragment.java
@@ -1,21 +1,33 @@
 package lcwu.fyp.petcaresystem.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigator;
 
+import android.view.FrameStats;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.jar.Attributes;
 
 import lcwu.fyp.petcaresystem.R;
+import lcwu.fyp.petcaresystem.director.Session;
+import lcwu.fyp.petcaresystem.model.User;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
+    private Session session;
+    private User user;
+    private TextView name, email, phone;
+    private TextView edit;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -27,7 +39,39 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        session = new Session(getActivity());
+        user = session.getUser();
+
+        name = v.findViewById(R.id.name);
+        email= v.findViewById(R.id.email);
+        phone= v.findViewById(R.id.phnNumber);
+        edit= v.findViewById(R.id.edit);
+
+        name.setText(user.getFirstName() + " " + user.getLastName());
+        email.setText(user.getEmail());
+        phone.setText(user.getPhNo());
+        edit.setOnClickListener(this);
+
+
         return  v;
     }
 
+    @Override
+    public void onClick(View view) {
+
+        int id = view.getId();
+
+        switch (id)
+        {
+            case R.id.edit:{
+                Intent it= new Intent( getActivity(), ProfileFragment.class);
+                startActivity(it);
+                break;
+
+            }
+        }
+
+
+
+    }
 }
